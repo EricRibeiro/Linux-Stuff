abfc0f335e440cbbdae1b06b6605ef1432bfb23c
Eric Ribeiro <ericribeiro@outlook.com.br>
2017-07-14 00:46:51 -0300

    fixed the closing before opening bug

--- startup-scripts/evolution-delay.sh
+++ startup-scripts/evolution-delay.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-(sleep 10s && evolution) &
+(sleep 10s && evolution) & sleep 60;
 pkill -f 'bash /home/eric/.startup-scripts/evolution-delay.sh'
--- startup-scripts/signal-delay.sh
+++ startup-scripts/signal-delay.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-(sleep 15s && chromium --disable-gpu --profile-directory=Default --app-id=bikioccmkafdpakkkcpdbppfkghcmihk) &
+(sleep 15s && chromium --disable-gpu --profile-directory=Default --app-id=bikioccmkafdpakkkcpdbppfkghcmihk) & sleep 60;
 pkill -f 'bash /home/eric/.startup-scripts/signal-delay.sh'
